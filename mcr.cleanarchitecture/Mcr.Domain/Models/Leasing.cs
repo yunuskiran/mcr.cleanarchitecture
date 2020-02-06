6ca56498a2932232ff345b0c499293ff176711ea
Yunus KIRAN <yunuskiran54@gmail.com>
2020-02-06 18:35:50 +0300

    domain and leasing object added

---
+++ mcr.cleanarchitecture/Mcr.Domain/Models/Leasing.cs
@@ -0,0 +1,10 @@
+﻿namespace Mcr.Domain.Models
+{
+    public class Leasing
+    {
+        public int Id { get; set; }
+        public int WorkorderId { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+    }
+}
