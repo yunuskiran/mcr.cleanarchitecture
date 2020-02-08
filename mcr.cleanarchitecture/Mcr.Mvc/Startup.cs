ad38d2ca01435c52df50c425dcb5d9013e34c782
Yunus KIRAN <yunuskiran54@gmail.com>
2020-02-08 21:51:04 +0300

    .

--- mcr.cleanarchitecture/Mcr.Mvc/Startup.cs
+++ mcr.cleanarchitecture/Mcr.Mvc/Startup.cs
@@ -44,7 +44,6 @@ namespace Mcr.Mvc
             Bootstrapper.RegisterServices(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
