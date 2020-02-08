0693da3bf70283af18218013fa590539e8c97696
Yunus KIRAN <yunuskiran54@gmail.com>
2020-02-08 14:25:08 +0300

    view added

---
+++ mcr.cleanarchitecture/Mcr.Mvc/Controllers/LeasingController.cs
@@ -0,0 +1,20 @@
+﻿using Mcr.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mcr.Mvc.Controllers
+{
+    public class LeasingController : Controller
+    {
+        private ILeasingService _leasingService;
+        public LeasingController(ILeasingService leasingService)
+        {
+            _leasingService = leasingService;
+        }
+
+        public IActionResult Index()
+        {
+            var leasingViewModel = _leasingService.GetLeasings();
+            return View(leasingViewModel);
+        }
+    }
+}
