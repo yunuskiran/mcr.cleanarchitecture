6186f3b98af9ec9476b54fd67fda74b6e41e8096
Yunus KIRAN <yunuskiran54@gmail.com>
2020-02-09 13:05:20 +0300

    cross cutting concerns added

---
+++ mcr.cleanarchitecture/Mcr.CrossCuttingConcerns/Extensions/GuidExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mcr.CrossCuttingConcerns.Extensions
+{
+    public static class GuidExtensions
+    {
+        public static bool IsNullOrEmpty(this Guid? guid)
+        {
+            return guid == null || guid == Guid.Empty;
+        }
+    }
+}
---
+++ mcr.cleanarchitecture/Mcr.CrossCuttingConcerns/Extensions/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Mcr.CrossCuttingConcerns.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static string AsJsonString(this object obj)
+        {
+            var content = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            return content;
+        }
+    }
+}
